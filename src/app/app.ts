4aafb41ca0992ca96ced1e75121a8336ce433dde
JorgeHSLA <jorgesierralaiton@gmail.com>
2025-10-05 01:55:40 -0500

    comenzamos con header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, Header, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
